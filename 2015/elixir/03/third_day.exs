ab1c9941337fd8a4e07c7bfb562cf5ab63a04ee2
Rasmus Larsson <rasmus.larsson@gmail.com>
2015-12-08 20:41:30 +0100

    Third day of Elixir Christmas.

--- 2015/elixir/03/third_day.exs
+++ 2015/elixir/03/third_day.exs
@@ -6,12 +6,34 @@ defmodule ThirdDay do
     Enum.uniq(locations) |> Enum.count
   end
 
+  def robo_navigate(instruction) do
+    list = String.to_char_list(instruction)
+    {santa, robo} = split_instructions list
+    santa_loc = parse(santa, {0,0}, [])
+    robo_loc = parse(robo, {0,0}, [])
+    (santa_loc ++ robo_loc) |> Enum.uniq |> Enum.count
+  end
+
+  def split_instructions(list) do
+    split_instructions(list, [], [])
+  end
+
+  def split_instructions([], santa, robo) do
+    {Enum.reverse(santa), Enum.reverse(robo)}
+  end
+
+  def split_instructions([s|[r|tail]], santa, robo) do
+    santa = [s] ++ santa
+    robo = [r] ++ robo
+    split_instructions tail, santa, robo
+  end
+
   def parse([], curr_loc, history) do
-    curr_loc ++ history
+    [curr_loc] ++ history
   end
 
   def parse([head|rest], curr_loc, history) do
-    new_history = curr_loc ++ history
+    new_history = [curr_loc] ++ history
     new_curr_loc = do_op(head, curr_loc)
     parse(rest, new_curr_loc, new_history)
   end
